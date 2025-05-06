ad1c637aaa026666898d3db636bdcd825ea52450
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2025-05-06 15:42:45 -0700

    FEAT: translation animation

---
+++ include/TranslationAnimation.h
@@ -0,0 +1,18 @@
+//
+// Created by Diego Cid on 5/6/25.
+//
+
+#ifndef TRANSLATIONANIMATION_H
+#define TRANSLATIONANIMATION_H
+class TranslationAnimation : public Animation {
+    glm::vec3 m_perSecond;
+    glm::vec3& m_objectPosition;
+    void applyAnimation(float dt) override {
+        m_objectPosition += m_perSecond * dt;
+    };
+public:
+    TranslationAnimation(glm::vec3& objectPosition, float duration, const glm::vec3& totalPosition) :
+       Animation(duration), m_objectPosition(objectPosition), m_perSecond(totalPosition / duration) {}
+};
+
+#endif //TRANSLATIONANIMATION_H
